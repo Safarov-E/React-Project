b927622535152c7f3cb6d2e2189b32e63cdc2d04
Elkhan <elkhan.safarov10@yandex.ru>
2019-05-04 20:30:30 +0500

    Updates

---
+++ precache-manifest.539664034c8a4d3fb7e8f25e61e5f445.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca9c2ad871e9554d43a29ed9828834b7",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "934a727295be07db94ab",
+    "url": "/react-project/static/js/2.85787108.chunk.js"
+  },
+  {
+    "revision": "dab8f0eebf6f4e7bf267",
+    "url": "/react-project/static/js/main.340f9fb2.chunk.js"
+  },
+  {
+    "revision": "c38e84bee75be2db3df6",
+    "url": "/react-project/static/js/runtime~main.270f79fb.js"
+  }
+]);
